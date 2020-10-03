bae3da02414ca463c729aea6ad47fd6f57b54418
trastolillo <trastolillo@tutanota.com>
2020-10-03 15:13:03 +0200

    inicio 158

--- Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -7,6 +7,35 @@ function Seguro(marca, year, tipo) {
   this.tipo = tipo;
 }
 
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function () {
+  let cantidad;
+  const base = 2000;
+  switch (this.marca) {
+    case '1':
+      cantidad = base * 1.15;
+      break;
+    case '2':
+      cantidad = base * 1.05;
+      break;
+    case '3':
+      cantidad = base * 1.35;
+      break;
+    default:
+      break;
+  }
+  // Por cada año que pasa el coche se devalúa un 3 por ciento
+  const diferencia = new Date().getFullYear() - this.year;
+  cantidad -= (diferencia * 3 * cantidad) / 100;
+  // El tipo de seguro también varía el precio
+  if (this.tipo === 'basico') {
+    cantidad *= 1.3;
+  } else {
+    cantidad *= 1.5;
+  }
+  return cantidad;
+};
+
 function UI() {}
 
 // Llena las opciones de los años
@@ -39,6 +68,42 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
   }, 3000);
 };
 
+UI.prototype.mostrarResultado = (total, seguro) => {
+  const { marca, year, tipo } = seguro;
+  let textoMarca;
+  switch (marca) {
+    case '1':
+      textoMarca = 'Americano';
+      break;
+    case '2':
+      textoMarca = 'Asiático';
+      break;
+    case '3':
+      textoMarca = 'Europeo';
+      break;
+    default:
+      break;
+  }
+  // Crear el resultado
+  const div = document.createElement('div');
+  div.classList.add('mt-10');
+  div.innerHTML = `
+  <p class='header'>Tu resumen</p>
+  <p class='font-bold'>Marca: <span class='font-normal'>${textoMarca}</span></p>
+  <p class='font-bold'>Año: <span class='font-normal'>${year}</span></p>
+  <p class='font-bold'>Tipo: <span class='font-normal capitalize'>${tipo}</span></p>
+  <p class='font-bold'>Total: <span class='font-normal'>\$${total}</span></p>
+  `;
+  const resultadoDiv = document.querySelector('#resultado');
+  // Mostrar spinner
+  const spinner = document.querySelector('#cargando');
+  spinner.style.display = 'block';
+  setTimeout(() => {
+    spinner.style.display = 'none';
+    resultadoDiv.appendChild(div);
+  }, 3000);
+};
+
 // Instanciar UI
 const ui = new UI();
 
@@ -62,7 +127,14 @@ function cotizarSeguro(e) {
     return;
   }
   ui.mostrarMensaje('Cotizando...', 'exito');
+  // Ocultar las cotizaciones previas
+  const resultados = document.querySelector('#resultado div');
+  if (resultados != null) {
+    resultados.remove();
+  }
   // Instanciar el seguro
-
+  const seguro = new Seguro(marca, year, tipo);
+  const total = seguro.cotizarSeguro();
   // Utilizar el prototype que va a cotizar
+  ui.mostrarResultado(total, seguro);
 }
